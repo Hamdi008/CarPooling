635f0fdf428362319af6d18d9fe403b9a52515cd
Hamdi008 <hamdi.elfekih@outlook.com>
2025-04-28 09:11:29 +0100

    Provide carpooling_database.db dataBase

--- app/src/main/java/com/hamdi/carpooling/dataBase/remote/UserRepositoryImpl.kt
+++ app/src/main/java/com/hamdi/carpooling/dataBase/remote/UserRepositoryImpl.kt
@@ -1,17 +1,31 @@
 package com.hamdi.carpooling.dataBase.remote
 
+import android.util.Log
+import com.hamdi.carpooling.dataBase.roomDB.AppDatabase
 import com.hamdi.carpooling.dataBase.roomDB.User
 import com.hamdi.carpooling.dataBase.roomDB.UserDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val api: UserApi,
-    private val dao: UserDao
+    //private val dao: UserDao,
+    private val db: AppDatabase
 ) : UserRepository {
     override suspend fun refreshUsers() {
         val usersFromApi = api.getUsers()
-        dao.insertUsers(usersFromApi)
+        Log.d("HEL:","usersFromApi = $usersFromApi")
+        withContext(Dispatchers.IO){
+            db.userDao().insertUsers(usersFromApi)
+        }
     }
 
-    override suspend fun getUsersFromDb(): List<User> = dao.getAllUsers()
+    override suspend fun getUsersFromDb(): List<User> {
+        val users = withContext(Dispatchers.IO) {
+            db.userDao().getAllUsers()
+        }
+        Log.d("UserRepository", "Fetched users from DB: $users")
+        return users
+    }
 }
